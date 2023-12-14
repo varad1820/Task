8c514533078d90627a19a4d53368db18dd1da915
varad1820 <dongarevarad2000@gmail.com>
2023-12-14 18:58:16 +0530

    added new lib function

---
+++ Calcultor/lib/Bool/Boolean.cpp
@@ -0,0 +1,12 @@
+#include "Mod.h"
+#include<iostream>
+
+bool BoolCheck(float a, float b)
+{   
+    if( a>b){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
---
+++ Calcultor/lib/Bool/Boolean.h
@@ -0,0 +1,6 @@
+#ifndef BOOLEAN_H
+#define BOOLEAN_H
+
+bool BoolCheck(float a,float b);
+
+#endif // MOD_H
